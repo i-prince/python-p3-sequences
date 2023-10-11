cd5527333c3853918089cc3a9e0a770e1b84981a
i-prince <molatonny@gmail.com>
2023-10-12 01:06:06 +0300

    completed assignment

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    a, b = 0, 1
+    count = 0
+
+    while count < length:
+        print(a, end=" ")
+        a, b = b, a + b
+        count += 1
+
+
+# Call the function with the desired length
+length = 10  # Set the desired length here
+print_fibonacci(length)
